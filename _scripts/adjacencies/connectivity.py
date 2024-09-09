1d7c4a3dc2b8f9f9147a58b0a26d39963f614386
Juliet Nwagwu Ume-Ezeoke <jnwagwu@stanford.edu>
2024-09-09 14:32:29 -0700

    changes

--- _scripts/adjacencies/connectivity.py
+++ _scripts/adjacencies/connectivity.py
@@ -9,26 +9,26 @@ import networkx as nx
 
 from svg_helpers.directions import Direction
 from svg_helpers.positioned_graph import PositionedGraph
+from svg_helpers.helpers import get_bounds_of_layout
 
+# todo for amber plan 01
 LINKED_EDGES = [0, 3, 6, 10, 12, 16, 18, 19, 20, 21]
 
-
-class WindowType(Enum):
-    A = 0
-    B = 1
-
+class SubsurfaceObjectType(Enum):
+    DOOR = 0
+    WINDOW = 1
 
 @dataclass
-class Window:
-    direction: Direction
-    window_type: WindowType
+class Subsurface:
+    door_or_window: SubsurfaceObjectType
+    id: int
 
     def to_json(self):
         return {
-            "direction": self.direction.name,
-            "window_type": self.window_type.name
+            "door_or_window": self.door_or_window.name,
+            "id": self.id
         }
-
+    
 
 @dataclass
 class Edge:
@@ -39,47 +39,62 @@ class Edge:
 class ConnectivityGenerator:
     def __init__(self, pos_graph: PositionedGraph) -> None:
         self.G_init = pos_graph.G
-        self.layout = pos_graph.layout
+        self.layout = deepcopy(pos_graph.layout)
         self.G = deepcopy(pos_graph.G)
 
     def run(self):
+        self.add_direction_nodes()
         self.add_windows()
         self.create_connectivity_template()
         self.update_connectivity_template()
         self.form_connectivity_graph()
-        self.write_to_file()
+        # self.write_to_file()
+
+    def add_direction_nodes(self):
+        for d in Direction:
+            self.G.add_node(d.name, direction_node=True)
+        self.update_layout_for_direction_nodes()
+
+        self.G_rooms = nx.subgraph_view(self.G, filter_node=self.filter_direction_nodes)
+
 
     def add_windows(self):
         # TODO this may change for other floor plans..
-        for key, data in self.G.nodes(data=True):
-            data["windows"] = []
+        for key, data in self.G_rooms.nodes(data=True):
+            # data["windows"] = []
             empty_dir = data["data"].get_empty_directions()
             for d in empty_dir:
                 if d == Direction.NORTH.name or d == Direction.SOUTH.name:
-                    data["windows"].append(Window(Direction[d], WindowType.A))
+                    self.G.add_edge(key, Direction[d].name, data=Subsurface(SubsurfaceObjectType.WINDOW, 1))
+                    
+                    # data["windows"].append(Window(Direction[d], WindowType.A))
 
     def create_connectivity_template(self):
         self.conn: Dict[int, Edge] = {}
-        for ix, e in enumerate(self.G.edges):
+        for ix, e in enumerate(self.G_rooms.edges):
             self.conn[ix] = Edge(e, False)
 
     def update_connectivity_template(self, linked_edges: list[int] = LINKED_EDGES):
         for ix in linked_edges:
             self.conn[ix].is_linked = True
 
-    def test_edge(self, e):
-        return frozenset(e) in [frozenset(v.edge) for v in self.conn.values()]
-
-    # def run_tests(self, test_fx: list[Callable[[Dict[int, Edge]], bool]]):
-    #     for fx in test_fx:
-    #         fx(self.conn)
-
     def form_connectivity_graph(self):
-        for e in self.G.edges:
+        for e in self.G_rooms.edges:
             if e not in [v.edge for v in self.conn.values() if v.is_linked == True]:
                 self.G.remove_edge(*e)
+            else:
+                self.G.edges[e]["data"] = Subsurface(SubsurfaceObjectType.DOOR, 7)
 
         assert len(self.G.edges()) <= len(self.G_init.edges)
+
+    def update_id(self, edges: list[tuple], id: int):
+        for e in edges:
+            try:
+                self.G.edges[e]["data"].id = id
+            except KeyError:
+                print(f"invalid edge - {e}")
+                pass
+            
 
     def draw_graph(self):
         nx.draw(self.G, self.layout, with_labels=True)
@@ -97,4 +112,37 @@ class ConnectivityGenerator:
             item["data"] = item["data"].to_json()
             item["windows"] = [i.to_json() for i in item["windows"]]
 
+
+######## direction stuff
+
+    def filter_direction_nodes(self, node):
+        try:
+            self.G.nodes[node]["direction_node"]
+            return False
+        except:
+            return True
+        
+
+    def update_layout_for_direction_nodes(self):
+        c = get_bounds_of_layout(self.layout)
+        print(c)
+        mid_x = ((c.x_right - c.x_left)/2) + c.x_left
+        mid_y = ((c.y_top - c.y_bottom)/2) + c.y_bottom
+        print(mid_x, mid_y)
+
+        PAD = 100
+
+        self.layout[Direction.NORTH.name] = (mid_x, c.y_top+PAD)
+        self.layout[Direction.SOUTH.name] = (mid_x, c.y_bottom-PAD)
+        self.layout[Direction.EAST.name] = (c.x_left-PAD,mid_y)
+        self.layout[Direction.WEST.name] = (c.x_right+PAD,mid_y)
+
     
+
+
+    # def test_edge(self, e):
+    #     return frozenset(e) in [frozenset(v.edge) for v in self.conn.values()]
+
+    # def run_tests(self, test_fx: list[Callable[[Dict[int, Edge]], bool]]):
+    #     for fx in test_fx:
+    #         fx(self.conn)
--- _scripts/svg_helpers/helpers.py
+++ _scripts/svg_helpers/helpers.py
@@ -1,5 +1,6 @@
 from typing import Dict,  Union
 import json
+from svg_helpers.domains import Corners
 
 def key_from_value(dict:Dict, val):
     return list(dict.keys())[list(dict.values()).index(val)]
@@ -10,3 +11,16 @@ def compare_sequences(t1: Union[tuple, list], t2: Union[tuple, list]):
 
 def toJson(obj):
         return json.dumps(obj, default=lambda o: o.__dict__)
+
+
+def get_bounds_of_layout(coordinates: Dict[str, tuple]):
+        x_values = [coord[0] for coord in coordinates.values()]
+        y_values = [coord[1] for coord in coordinates.values()]
+
+
+        x_min = min(x_values)
+        x_max = max(x_values)
+        y_min = min(y_values)
+        y_max = max(y_values)
+
+        return Corners(x_min, x_max, y_min, y_max)
