3e07d5673ce335d15acabc60ef746b265d46bfb1
Juliet Nwagwu Ume-Ezeoke <jnwagwu@stanford.edu>
2024-10-11 10:40:56 -0700

    connectivity graph

--- _scripts/interactive/entry.py
+++ _scripts/interactive/entry.py
@@ -12,10 +12,16 @@ import typer
 from rich import print as rprint
 from rich.prompt import Confirm, Prompt
 from placement2.connectivity import create_cardinal_dags
+from typing import Literal, NamedTuple
 
 SVG_PATHS =  "/Users/julietnwagwuume-ezeoke/_UILCode/gqe-phd/svg2plan/svg_imports"
 
 JSON_PATH = "test.json"
+
+class DetailedEdge(NamedTuple):
+    pair: tuple[str, str]
+    stype: Literal["DOOR", "WINDOW"]
+    id: str
 
 def complete_case(incomplete: str):
     case_names = os.listdir(SVG_PATHS)
@@ -29,36 +35,6 @@ def stringify(ix, e):
     u,v = e
     return f"{ix}. {u} - {v}"
 
-
-def is_window_edge(e: tuple[str, str]):
-    u,v = e
-    drns = [i.name for i in Direction]
-    if u in drns or v in drns:
-        return True
-    
-
-def assign_details(connections: list[tuple[str, str]]):
-    # TODO make dict type... 
-    with open(JSON_PATH, "r") as file:
-        details = json.load(file)
-
-    window_types = details["WINDOWS"]
-    door_types = details["DOORS"]
-    window_ids = [str(i["id"]) for i in window_types]
-    door_ids = [str(i["id"]) for i in door_types]
-
-    responses = []
-    for ix, e in enumerate(connections):
-        if is_window_edge(e):
-            r = Prompt.ask(stringify(ix, e), choices = window_ids)
-            stype = "WINDOW"
-        else:
-            r = Prompt.ask(stringify(ix, e), choices = door_ids)
-            stype = "DOOR"
-        responses.append((e, stype, r))
-    rprint("assigned edges", responses)
-    return responses
-
 def assess_edges(G: nx.DiGraph):
     n_edges = len(list(G.edges))
     rprint(f"Number of edges to assess: {n_edges}")
@@ -70,12 +46,46 @@ def assess_edges(G: nx.DiGraph):
     rprint("valid_edges:", valid_edges)
     return valid_edges
 
+def is_window_edge(e: tuple[str, str]):
+    u,v = e
+    drns = [i.name for i in Direction]
+    if u in drns or v in drns:
+        return True
+    
+def assign_details(connections: list[tuple[str, str]]):
+    # TODO make dict type... 
+    with open(JSON_PATH, "r") as file:
+        details = json.load(file)
+
+    window_types = details["WINDOWS"]
+    door_types = details["DOORS"]
+    window_ids = [str(i["id"]) for i in window_types]
+    door_ids = [str(i["id"]) for i in door_types]
+
+    responses:list[DetailedEdge] = []
+    for ix, e in enumerate(connections):
+        if is_window_edge(e):
+            id = Prompt.ask(stringify(ix, e), choices = window_ids)
+            stype = "WINDOW"
+        else:
+            id = Prompt.ask(stringify(ix, e), choices = door_ids)
+            stype = "DOOR"
+        responses.append(DetailedEdge(e, stype, id))
+    rprint("assigned edges", responses)
+    return responses
+
+
 def handle_edges(G: nx.DiGraph):
     valid_edges = assess_edges(G)
     rprint("**** Beginning to assign edges ****")
     return assign_details(valid_edges)
 
-    
+
+def create_json(G:nx.Graph):
+    G_json = nx.node_link_data(G)
+    path = os.path.join("graph.json")
+    with open(path, "w+") as file:
+        json.dump(G_json, default=str, fp=file)
 
 
 
@@ -86,16 +96,31 @@ def read_svg(case_name: Annotated[str, typer.Argument(help="case_name", autocomp
     rprint(f"Reading {case_name}")
     sv = SVGReader(case_name)
     sv.run()
-    domains, [Gx, Gy] = adjust_domains(sv.layout.domains)
+    _, [Gx, Gy] = adjust_domains(sv.layout.domains)
     Gxd, Gyd = create_cardinal_dags(Gx, Gy)
 
     rprint("\n ---- Beginning to assess X edges -----")
     x_edges = handle_edges(Gxd)
+    x_edges = []
 
     rprint("\n ---- Beginning to assess Y edges -----")
     y_edges = handle_edges(Gyd)
 
-    # TODO: write to connectivity graph, pass graph to leveler.. 
+    edges = x_edges + y_edges
+
+    Gconn = nx.DiGraph()
+
+    for e in edges:
+        [u,v], stype, id = e
+        Gconn.add_edge(u,v, details={"stype": stype, "id": id})
+
+    create_json(Gconn)
+
+    rprint("Saved json file")
+
+    return 
+
+ 
 
 
     
